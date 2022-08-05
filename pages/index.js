e15314562ca6af14a1b3a5a6d5de92292de268ed
quang.nv212 <95917117+Quang-212@users.noreply.github.com>
2022-08-05 13:34:02 +0700

    Add dynamic input

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Fragment, useEffect, useState } from "react";
 import { createProduct } from "../fetching/product";
+import { useFieldArray, useForm } from "react-hook-form";
 
 export default function Home() {
   const [picture, setPicture] = useState({});
@@ -8,7 +9,27 @@ export default function Home() {
   const handleUpload1 = (event, index) => {
     setPicture((prev) => ({ ...prev, [index]: event.target.files[0] }));
   };
-
+  const { control, register, handleSubmit } = useForm({
+    defaultValues: {
+      size: ["s"],
+    },
+  });
+  const {
+    fields: size_field,
+    append: addSize,
+    remove: rmSize,
+  } = useFieldArray({
+    control, // control props comes from useForm (optional: if you are using FormContext)
+    name: "size", // unique name for your Field Array
+  });
+  const {
+    fields: pic_field,
+    append: addPic,
+    remove: rmPic,
+  } = useFieldArray({
+    control, // control props comes from useForm (optional: if you are using FormContext)
+    name: "file", // unique name for your Field Array
+  });
   const handleUpload = async () => {
     const upload = await Promise.all(
       Object.entries(picture).map(([_, picture]) => {
@@ -37,26 +58,82 @@ export default function Home() {
   const handleCheckPrimaryPicture = (event) => {
     setPrimaryPicture(+event.target.value);
   };
+
   return (
-    <div>
-      {[...Array(4)].map((_, index) => (
-        <Fragment key={index}>
-          <input
-            type="checkbox"
-            name="primary"
-            value={index}
-            checked={primaryPicture === index}
-            onChange={handleCheckPrimaryPicture}
-          />
-          <input
-            type="file"
-            onChange={(event) => {
-              handleUpload1(event, index);
-            }}
-          />
+    <form
+      onSubmit={handleSubmit((data) =>
+        console.log({ ...data, file: data.file.map((file) => file[0]) })
+      )}
+    >
+      <div>
+        <label>Name</label>
+        <input {...register("name")} />
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <label>Image</label>
+        <div>
+          <button type="button" onClick={() => addPic(null)}>
+            Add Image
+          </button>
+          {pic_field.map(({ id }, index) => (
+            <div key={id}>
+              <input
+                type="checkbox"
+                name="primary"
+                value={index}
+                checked={primaryPicture === index}
+                onChange={handleCheckPrimaryPicture}
+              />
+              <input type="file" {...register(`file[${index}]`)} />
+              <button type="button" onClick={() => rmPic(index)}>
+                Rm Image
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* {pic_field.map(({ id }, index) => (
+        <Fragment key={id}>
+          <div>
+            <label>Image</label>
+
+            <div>
+              <button type="button" onClick={() => addSize("")}>
+                Add Image
+              </button>
+              <input
+                type="checkbox"
+                name="primary"
+                value={index}
+                checked={primaryPicture === index}
+                onChange={handleCheckPrimaryPicture}
+              />
+              <input type="file" {...register(`file[${index}]`)} />
+              <button type="button" onClick={() => rmSize(index)}>
+                Rm image
+              </button>
+            </div>
+          </div>
         </Fragment>
-      ))}
-      <button onClick={handleUpload}>Upload</button>
-    </div>
+      ))} */}
+      <div style={{ display: "flex" }}>
+        <label>Size</label>
+        <div>
+          <button type="button" onClick={() => addSize("")}>
+            Add size
+          </button>
+          {size_field.map(({ id }, index) => (
+            <div key={id}>
+              <input {...register(`size[${index}]`)} />
+              <button type="button" onClick={() => rmSize(index)}>
+                Remove size
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+      <button>Upload</button>
+    </form>
   );
 }
